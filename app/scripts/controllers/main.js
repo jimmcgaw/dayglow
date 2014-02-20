29f1ea9a4c349dd563ee6b6d3598ced5aba30053
Jim McGaw <jim@rightscale.com>
2014-02-20 14:02:58 -0800

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('dayglowApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
